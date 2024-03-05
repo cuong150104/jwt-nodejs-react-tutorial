1199c7e3885252b136c90142397fb71f01479920
DucCuong <nguyenduccuong01152004@gmail.com>
2024-03-06 00:09:56 +0700

    video 7 tao table bang migrate

--- src/migrations/migrate-role.js
+++ src/migrations/migrate-role.js
@@ -1,22 +1,22 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('User', {
+    await queryInterface.createTable('Role', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
+    
+      url: {
         type: Sequelize.STRING
       },
-      password: {
+      description: {
         type: Sequelize.STRING
       },
-      username: {
-        type: Sequelize.STRING
-      },
+      
+      
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('User');
+    await queryInterface.dropTable('Role');
   }
 };
--- src/service/userService.js
+++ src/service/userService.js
@@ -1,14 +1,14 @@
 import bcrypt from 'bcryptjs';
 import mysql from 'mysql2/promise';
 import bluebird from 'bluebird';
-
+import db from '../models/index';
 
 
 const salt = bcrypt.genSaltSync(10);
 
 
 
-const hashPassword =(userPassword)=>
+const hashUserPassword =(userPassword)=>
 {
     let hashPassword = bcrypt.hashSync(userPassword, salt);
     return hashPassword;
@@ -16,15 +16,17 @@ const hashPassword =(userPassword)=>
 
 const createNewUser =  async (email, password, username) =>
 {
-    let hashpass = hashPassword(password);
-    const connection = await mysql.createConnection({host: 'localhost',user: 'root',database: 'jwt',Promise: bluebird});
-
-       // A simple SELECT query
-
-
-    const [rows, fields] = 
-    await connection.execute('INSERT INTO user (email, password, username) VALUES (?, ?, ?)',
-    [email,hashpass,username]);
+    let hashpass = hashUserPassword(password);
+    try{
+       await db.User.create({
+            username: username,
+            email: email,
+            password: password,
+        })
+    }catch(error)
+    {
+        console.log(">> check error: ", error);
+    }
 
 }
 
